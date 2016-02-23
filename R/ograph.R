b8181233870b0bc3906d8f70e2f715bd1bbd523d
hxin <hxin87@gmail.com>
2016-02-23 16:26:36 +0000

    #

--- R/ograph.R
+++ R/ograph.R
@@ -7,12 +7,12 @@
   if("igraph" %in% rownames(installed.packages())){
     v<-packageVersion("igraph")
     if(!v=='0.7.1'){
-      message("igraph 0.7.1 is required!, Please install it by typing the following into your R prot!")
-      message(">install.packages('https://cran.r-project.org/src/contrib/Archive/igraph/igraph_0.7.1.tar.gz', repos=NULL, type='source')")
+      warning("igraph 0.7.1 is required!, Please install it by typing the following into your R prot!
+              install.packages('https://cran.r-project.org/src/contrib/Archive/igraph/igraph_0.7.1.tar.gz', repos=NULL, type='source')")
     }
   }else{
-    message("igraph 0.7.1 is required!, Please install it by typing the following into your R prot!")
-    message(">install.packages('https://cran.r-project.org/src/contrib/Archive/igraph/igraph_0.7.1.tar.gz', repos=NULL, type='source')")
+    warning("igraph 0.7.1 is required!, Please install it by typing the following into your R prot!
+              install.packages('https://cran.r-project.org/src/contrib/Archive/igraph/igraph_0.7.1.tar.gz', repos=NULL, type='source')")
   }
   
   
