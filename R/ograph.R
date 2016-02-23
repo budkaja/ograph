3323ae49f84f500c245eceec60d7e211d102c99c
hxin <hxin87@gmail.com>
2016-02-23 16:26:13 +0000

    adding a warning message if you do not have the required igraph 0.7.1 package install. The new igraph 1.0.1 somhow runs slow on function get.adjList when working with large graph.

--- R/ograph.R
+++ R/ograph.R
@@ -4,4 +4,18 @@
   # initVar(where) 
   ## some preprocessing
   init()
+  if("igraph" %in% rownames(installed.packages())){
+    v<-packageVersion("igraph")
+    if(!v=='0.7.1'){
+      message("igraph 0.7.1 is required!, Please install it by typing the following into your R prot!")
+      message(">install.packages('https://cran.r-project.org/src/contrib/Archive/igraph/igraph_0.7.1.tar.gz', repos=NULL, type='source')")
+    }
+  }else{
+    message("igraph 0.7.1 is required!, Please install it by typing the following into your R prot!")
+    message(">install.packages('https://cran.r-project.org/src/contrib/Archive/igraph/igraph_0.7.1.tar.gz', repos=NULL, type='source')")
+  }
+  
+  
+  
 }
+packageVersion("topOnto.HPO.db")
